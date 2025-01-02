265da84b6d15ed3d5a0d01859382b1efd9548cfd
Syed Danyal Raza <syeddanyalraza@outlook.com>
2025-01-02 23:41:43 +0500

    Updated Category.js

--- Web Project/front-end/src/pages/Category.js
+++ Web Project/front-end/src/pages/Category.js
@@ -1,28 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Alert } from "react-bootstrap";
 import { useParams} from "react-router-dom";
 import products from "../Data/Products";
 import ProductCard from "../components/ProductCard";
+import { useCart } from "../CartContext";
 
 const CategoryPage = () => {
   const { category } = useParams();
-
   const [categoryProducts, setCategoryProducts] = useState([]);
+  const { cartMessage } = useCart();
 
   useEffect(() => {
-    const filteredProducts = products.filter((product) => product.category.toLowerCase() === category.toLowerCase());
+    const filteredProducts = products.filter(
+      (product) => product.category.toLowerCase() === category.toLowerCase()
+    );
     setCategoryProducts(filteredProducts);
   }, [category]);
 
-
   return (
-    <div style={{backgroundColor: "#d9d9d9"}}>
+    <div style={{ backgroundColor: "#d9d9d9" }}>
       <Container className="py-5">
         <h2 className="text-center mb-4">{category} Products</h2>
+        {cartMessage && (
+          <Alert variant="success" className="text-center">
+            {cartMessage}
+          </Alert>
+        )}
+
         <Row className="justify-content-center">
           {categoryProducts.length > 0 ? (
             categoryProducts.map((product) => (
-              <Col key={product.id} xs={12} sm={6} md={4} lg={3} className="d-flex justify-content-center">
+              <Col
+                key={product.id}
+                xs={12}
+                sm={6}
+                md={4}
+                lg={3}
+                className="d-flex justify-content-center"
+              >
                 <ProductCard product={product} />
               </Col>
             ))
